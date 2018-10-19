5e70154fb9a96d13b015d449c353091f3e9ef364
Nikita <sheikinnikita@yandex.ru>
2018-10-19 15:33:03 +0300

    2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tgmath.h>
+
+int bar (int a, int b);
+double bar (double m, double t, double p);
+RES = u/(sqrt(2)*(3,14)*pow(o,2)*p*N*6,022*pow(10,23));
+return RES;
+int main() {
+    std::cout << bar << std::endl;
+    return 0;
+}
